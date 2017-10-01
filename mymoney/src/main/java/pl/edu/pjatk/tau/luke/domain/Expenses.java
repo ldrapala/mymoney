c21d4a828d2a4ee9bd9dc153651fc29ebdbb089d
s12165 <s12165@lab1l-6.pjwstk.edu.pl>
2017-10-01 10:54:24 +0200

    model created

---
+++ mymoney/src/main/java/pl/edu/pjatk/tau/luke/domain/Expenses.java
@@ -0,0 +1,57 @@
+package pl.edu.pjatk.tau.luke.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author s12165
+ */
+public class Expenses {
+    
+    private int id;
+    private int amount;
+    private Date date;
+    private String category;
+    private String details;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+}
